456a5c85826654d67c565c59ed8ac5bbf94e3e6d
antonms <a.marinsanchez91@gmail.com>
2024-02-14 12:27:17 +0100

    Add topic and topic views and templates

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,5 +7,9 @@ from . import views
 app_name = 'learning_logs'
 urlpatterns = [
     # Initial page
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # Page that shows all the topics
+    path('topics/', views.topics, name='topics'),
+    # Details for each topic
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
